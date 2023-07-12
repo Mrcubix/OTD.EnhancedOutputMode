bce1978f3cd3b9ace30fdb1b17d2a1fcea0c2b2b
Mrcubix <tintamare91@gmail.com>
2023-07-12 03:13:37 +0200

    Changed the build scripts

---
+++ build.ps1
@@ -0,0 +1,27 @@
+dotnet restore
+
+dotnet publish -c Release -o temp
+
+# if error is not 0, exit in powershell
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+# create a folder if it doesn't exist named "build"
+if (!(Test-Path build)) {
+    New-Item -ItemType Directory -Force -Path build
+}
+
+# remove existing files inside build
+Remove-Item build/* -Recurse
+
+Move-Item temp/OTD.EnhancedOutputMode.dll build/OTD.EnhancedOutputMode.dll
+Move-Item temp/OTD.EnhancedOutputMode.pdb build/OTD.EnhancedOutputMode.pdb
+Move-Item temp/OTD.EnhancedOutputMode.Lib.dll build/OTD.EnhancedOutputMode.Lib.dll
+Move-Item temp/OTD.EnhancedOutputMode.Lib.pdb build/OTD.EnhancedOutputMode.Lib.pdb
+Move-Item temp/VMulti.dll build/VMulti.dll
+Move-Item temp/VMulti.pdb build/VMulti.pdb
+
+Set-Location ./build
+
+7z a -r OTD.EnhancedOutputMode.zip *
+
+Set-Location ..
--- build.sh
+++ build.sh
@@ -1,1 +1,23 @@
-dotnet publish -c Release -o build
+#!/bin/bash
+
+dotnet restore
+
+dotnet publish -c Release -o temp $@ || exit 1
+
+# create a folder if it doesn't existr named "build"
+mkdir build
+
+rm -rf build/*
+
+mv temp/OTD.EnhancedOutputMode.dll build/OTD.EnhancedOutputMode.dll
+mv temp/OTD.EnhancedOutputMode.pdb build/OTD.EnhancedOutputMode.pdb
+mv temp/OTD.EnhancedOutputMode.Lib.dll build/OTD.EnhancedOutputMode.Lib.dll
+mv temp/OTD.EnhancedOutputMode.Lib.pdb build/OTD.EnhancedOutputMode.Lib.pdb
+mv temp/VMulti.dll build/VMulti.dll
+mv temp/VMulti.pdb build/VMulti.pdb
+
+cd ./build
+
+zip -r OTD.EnhancedOutputMode.zip *
+
+cd ..
